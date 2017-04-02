3e1a71089f7cb46c819db49859868d3df9f3c134
xavisson <javipalacio@gmail.com>
2017-04-02 23:21:29 +0200

    AsyncTask to request movies data

--- app/src/main/java/com/xavisson/popularmovies/MainActivity.java
+++ app/src/main/java/com/xavisson/popularmovies/MainActivity.java
@@ -1,13 +1,71 @@
 package com.xavisson.popularmovies;
 
-import android.support.v7.app.AppCompatActivity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.xavisson.popularmovies.utilities.NetworkUtils;
+
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loadMoviesData();
+
+    }
+
+    private void loadMoviesData() {
+        String sortBy = "popular";
+        new FetchMoviesTask().execute(sortBy);
+    }
+
+    public class FetchMoviesTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... strings) {
+
+            if (strings.length == 0) {
+                return null;
+            }
+
+            String sortBy = strings[0];
+            URL moviesRequestUrl = NetworkUtils.buildUrl(sortBy);
+            Log.d(LOG_TAG, "moviesRequestUrl: " + moviesRequestUrl);
+            Log.d(LOG_TAG, "static key: " + BuildConfig.MOVIES_API_KEY);
+
+            try {
+                String jsonMoviesResponse = NetworkUtils
+                        .getResponseFromHttpUrl(moviesRequestUrl);
+
+//                String[] simpleJsonMoviesData = OpenWeatherJsonUtils
+//                        .getSimpleWeatherStringsFromJson(MainActivity.this, jsonMoviesResponse);
+//
+//                return simpleJsonMoviesData;
+
+                return jsonMoviesResponse;
+
+            } catch (Exception e) {
+                Log.d(LOG_TAG, "Exception: " + e.getMessage());
+                e.printStackTrace();
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String moviesData) {
+            if (moviesData != null) {
+
+                Log.d(LOG_TAG, "onPost: " + moviesData);
+
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/xavisson/popularmovies/utilities/NetworkUtils.java
@@ -0,0 +1,75 @@
+package com.xavisson.popularmovies.utilities;
+
+import android.net.Uri;
+import android.util.Log;
+
+import com.xavisson.popularmovies.BuildConfig;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+
+/**
+ * Created by javidelpalacio on 2/4/17.
+ * These utilities will be used to communicate with the movies database.
+ */
+
+public class NetworkUtils {
+
+    private static final String TAG = NetworkUtils.class.getSimpleName();
+
+    private static final String MOVIES_URL = "https://api.themoviedb.org/3/movie/";
+
+    final static String QUERY_PARAM = "q";
+
+    final static String POPULAR_MOVIES = "popular";
+    final static String RECENT_MOVIES = "";
+    final static String TOP_RATED_MOVIES = "top_rated";
+    final static String PARAM_API_KEY = "api_key";
+
+    final static String myAPIKey = BuildConfig.MOVIES_API_KEY;
+
+
+//    http://api.themoviedb.org/3/movie/popular?api_key=d11cabc7185de71d4cbd15a1033a1a46
+
+    public static URL buildUrl(String sortBy) {
+
+        Uri builtUri = Uri.parse(MOVIES_URL).buildUpon()
+                .appendPath(sortBy)
+                .appendQueryParameter(PARAM_API_KEY, myAPIKey)
+                .build();
+
+        URL url = null;
+        try {
+            url = new URL(builtUri.toString());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        Log.d(TAG, "Built URI " + url);
+
+        return url;
+    }
+
+    public static String getResponseFromHttpUrl(URL url) throws IOException {
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        try {
+            InputStream in = urlConnection.getInputStream();
+
+            Scanner scanner = new Scanner(in);
+            scanner.useDelimiter("\\A");
+
+            boolean hasInput = scanner.hasNext();
+            if (hasInput) {
+                return scanner.next();
+            } else {
+                return null;
+            }
+        } finally {
+            urlConnection.disconnect();
+        }
+    }
+}
